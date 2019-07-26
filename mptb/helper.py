d74f5311aa489558923f1ff643da31768315d572
Masashi Kimura <kimura@convergence-lab.com>
2019-07-26 14:22:26 +0900

    fix helper

--- mptb/helper.py
+++ mptb/helper.py
@@ -200,5 +200,5 @@ class Helper(object):
             batch = tuple(t.to(self.device) for t in batch)
             with torch.no_grad():
                 _, predict = process(batch, model, iter_bar, step)
-                predicts.append({"pred":predict[0][0], "true": predict[1][0]})
+                predicts.append({"pred":predict[0][0], "true": predict[1][0][0]})
         return predicts
